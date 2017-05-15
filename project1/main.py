3690788a2e1282185b6a5e8d14f14c148feefbf2
lawben <lawrencebenson95@gmail.com>
2017-05-15 15:33:17 +0200

    Clean up a bit

--- project1/main.py
+++ project1/main.py
@@ -3,19 +3,19 @@ from algos import random_local_search, one_one_ea, one_lambda_ea
 
 
 N_STEP = 25
+N_MAX = 75
 ITERATIONS = 10
 
 
-def test_algo(algo, eval_fn, max_n):
+def test_algo(algo, eval_fn):
     n = N_STEP
     all_iterations = []
-    while n <= max_n:
+    for n in range(N_STEP, N_MAX + 1, N_STEP):
         iterations = [algo(eval_fn, n) for _ in range(ITERATIONS)]
         all_iterations.append(iterations)
         avg_iterations = sum(iterations) // len(iterations)
         print("Finnished: {} - {} - {} - {}".format(
             algo.__name__, eval_fn.__name__, n, avg_iterations))
-        n += N_STEP
 
     return all_iterations
 
@@ -27,7 +27,7 @@ def main():
 
     for algo in algos:
         for eval_fn in evals:
-            its += test_algo(algo, eval_fn, 50)
+            its += test_algo(algo, eval_fn)
 
 
 if __name__ == '__main__':
