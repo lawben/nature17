46bc109298722223cd033b4257f30d91d846efc3
lawben <lawrencebenson95@gmail.com>
2017-05-15 13:48:58 +0200

    Move testing to main

--- project1/algos.py
+++ project1/algos.py
@@ -2,7 +2,6 @@ import random
 
 from bitstring import BitArray
 
-from test_functions import one_max, jump, bin_val, royal_roads, leading_ones
 
 N_STEP = 25
 ITERATIONS = 10
@@ -15,7 +14,6 @@ def generate_x(n):
 def random_local_search(eval_fn, n):
     x = generate_x(n)
     score = eval_fn(x)
-    print(score)
     iterations = 0
     while score != n:
         y = x[:]
@@ -30,26 +28,3 @@ def random_local_search(eval_fn, n):
         iterations += 1
 
     return iterations
-
-
-def test_algo(algo, eval_fn, max_n):
-    n = N_STEP
-    all_iterations = []
-    while n <= max_n:
-        iterations = [algo(eval_fn, n) for _ in range(ITERATIONS)]
-        all_iterations.append(iterations)
-        n += N_STEP
-
-    return all_iterations
-
-
-def main():
-    its = []
-    for algo in [random_local_search]:
-        for eval_fn in [one_max, jump, bin_val, royal_roads, leading_ones]:
-            its += test_algo(algo, eval_fn, 50)
-    print(its)
-
-
-if __name__ == '__main__':
-    main()
---
+++ project1/main.py
@@ -0,0 +1,30 @@
+from bitstring import BitArray
+
+from test_functions import one_max, jump, bin_val, royal_roads, leading_ones
+from algos import random_local_search
+
+N_STEP = 25
+ITERATIONS = 10
+
+
+def test_algo(algo, eval_fn, max_n):
+    n = N_STEP
+    all_iterations = []
+    while n <= max_n:
+        iterations = [algo(eval_fn, n) for _ in range(ITERATIONS)]
+        all_iterations.append(iterations)
+        n += N_STEP
+
+    return all_iterations
+
+
+def main():
+    its = []
+    for algo in [random_local_search]:
+        for eval_fn in [one_max, jump, bin_val, royal_roads, leading_ones]:
+            its += test_algo(algo, eval_fn, 50)
+    print(its)
+
+
+if __name__ == '__main__':
+    main()
