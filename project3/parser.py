30ec3ee2be953590a7d6bf44488d5d0b68346ae9
lawben <mail@lawben.com>
2017-07-05 18:49:09 +0200

    Add 101 city dataset

--- project3/parser.py
+++ project3/parser.py
@@ -1,5 +1,9 @@
+import math
 import sys
 import re
+
+import numpy as np
+
 
 def get_int_from_line(line):
     return int(re.search("(\d+)", line).group(0))
@@ -11,25 +15,13 @@ def get_dimension(raw_lines):
             return get_int_from_line(line)
 
 
-def parse(data_file):
-    raw_data = [line for line in open(data_file)]
-    matrix_start = -1
-    dimension = get_dimension(raw_data)
+def parse_node_points(raw_data, points_start, dimension):
+    points = []
+    for i in range(points_start, points_start + dimension):
+        _, x, y = [float(x) for x in raw_data[i].split()]
+        points.append((x, y))
 
-    for i, line in enumerate(raw_data):
-        if line.startswith("EDGE_WEIGHT_SECTION"):
-            matrix_start = i + 1
-            break
-
-    if matrix_start == -1 or dimension == -1:
-        raise ValueError("File does not contain matrix or dimension!")
-
-    matrix = []
-    for i in range(matrix_start, matrix_start + dimension):
-        data_row = [int(x) for x in raw_data[i].split()]
-        matrix.append(data_row)
-
-    return matrix
+    return points
 
 
 def parse_points(data_file):
@@ -38,15 +30,55 @@ def parse_points(data_file):
 
     for i, line in enumerate(raw_data):
         if line.startswith("DISPLAY_DATA_SECTION"):
-            points_start = i + 1
-            break
+            return parse_node_points(raw_data, i + 1, dimension)
 
-    points = []
-    for i in range(points_start, points_start + dimension):
-        _, x, y = [float(x) for x in raw_data[i].split()]
-        points.append((x, y))
+        if line.startswith("NODE_COORD_SECTION"):
+            return parse_node_points(raw_data, i + 1, dimension)
 
-    return points
+    raise ValueError("Cannot parse points from file!")
+
+
+def parse_matrix(raw_data, matrix_start, dimension):
+    matrix = []
+    for i in range(matrix_start, matrix_start + dimension):
+        data_row = [int(x) for x in raw_data[i].split()]
+        matrix.append(data_row)
+
+    return matrix
+
+
+def parse_node_coords(raw_data, matrix_start, dimension):
+    info = parse_node_points(raw_data, matrix_start, dimension)
+
+    n = len(info)
+    matrix = np.empty((n, n))
+
+    for i in range(n):
+        x1, y1 = info[i]
+        for j in range(i, n):
+            x2, y2 = info[j]
+            dist = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+            matrix[i, j] = matrix[j, i] = dist
+
+    return matrix
+
+
+def parse(data_file):
+    raw_data = [line for line in open(data_file)]
+    matrix_start = -1
+    dimension = get_dimension(raw_data)
+
+    if dimension == -1:
+        raise ValueError("File does not contain dimension!")
+
+    for i, line in enumerate(raw_data):
+        if line.startswith("EDGE_WEIGHT_SECTION"):
+            return parse_matrix(raw_data, i + 1, dimension)
+
+        if line.startswith("NODE_COORD_SECTION"):
+            return parse_node_coords(raw_data, i + 1, dimension)
+
+    raise ValueError("File does not contain relevant information")
 
 
 def parse_tour(data_file):
